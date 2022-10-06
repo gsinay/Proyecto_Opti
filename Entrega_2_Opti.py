e7b14db4d120c4f6610a5a7e9c8ae59272d289fb
Gabriel Sinay <gsinayc@gmail.com>
2022-10-06 16:52:34 -0300

    añadí mas a la estructura

--- Entrega_2_Opti.py
+++ Entrega_2_Opti.py
@@ -1,9 +1,14 @@
 from gurobipy import Model, GRB, quicksum
 
 #Conjuntos
-T = range(53) #Semanas del año
+T = range(53) #Semanas del año 
+Tr = [1,2] #Tipos de trigo, 1= HRW, 2=SRW
+# S = ACA HAY QUE RELLENAR DEPENDIENDO DE LOS DATOS!!!
+
 
 #Variables
+
+#Parámetros (algunos hay que leer de la instancia de datos y hacerlos una lista que correspondan a los indices de sus conjuntos, otros son numeros nomas)
 
 #Por acá hay que poner el time limit, no me acuerdo como era
 
