134961095a45ed0addfc7d02081eab466f738fcf
Gabriel Sinay <gsinayc@gmail.com>
2022-10-06 16:43:27 -0300

    backbone

--- Entrega_2_Opti.py
+++ Entrega_2_Opti.py
@@ -1,1 +1,15 @@
-#tratando
+from gurobipy import Model, GRB, quicksum
+
+#Conjuntos
+
+#Variables
+
+#Por acá hay que poner el time limit, no me acuerdo como era
+
+#updatear
+model.update()
+
+
+#R2-R16: restricciones de enunciado
+
+#Función Objetivo
